7401976a4aa951201727b9f730bff747b667e8fe
Evelyn.Bautista <evelyn.bautista@le-campus-numerique.fr>
2025-07-02 13:50:24 +0200

    MY 2 scripts

---
+++ models/Staging/stg_stateprovince.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table', schema='_staging') }}
+
+SELECT
+  stateprovinceid, 
+  countryregioncode,
+  territoryid,
+  name AS regionname,
+  stateprovincecode
+FROM {{ source('raw', 'stateprovince') }}
+
+--run in terminal/shell: 
+--dbt run --select stg_stateprovince
+
+--(This will build the model as a new view/table in your dev schema, leaving the original data untouched)
